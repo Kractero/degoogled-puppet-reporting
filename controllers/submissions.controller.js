7b0c78a66002b5f08fcb7c3019425e34fd77975a
Kractero <kractero@sideroca.com>
2024-01-15 19:21:05 -0700

    ui adjustments

--- controllers/submissions.controller.js
+++ controllers/submissions.controller.js
@@ -1,9 +1,10 @@
-import { db } from "../util/db.js";
+import { db, displaySubmissions } from "../util/db.js";
 import { encoded } from "../util/encoded.js";
 
 export const submit = (req, res) => {
   if (req.query.main && req.query.puppets) {
-    const result = db.prepare('INSERT OR IGNORE INTO submissions VALUES (?, ?);').run([encoded(req.query.main), encoded(req.query.puppets.replaceAll('\r', '').replaceAll('\n', ','))]);
+    const cleanPuppets = req.query.puppets.split('\n').map((str) => str.replace(/\r/g, '')).filter((str) => str.trim() !== '').join(',')
+    const result = db.prepare('INSERT OR IGNORE INTO submissions VALUES (?, ?, ?);').run([encoded(req.query.main), encoded(cleanPuppets), Math.floor(Date.now() / 1000)]);
     res.render("submit", { success: result.changes > 0 ? "Successfully added to submission queue." : "Failed to submit." });
   } else {
     res.render("submit", { success: "" });
@@ -11,11 +12,5 @@ export const submit = (req, res) => {
 }
 
 export const getSubmissions = (req, res) => {
-  const submissions = db.prepare('SELECT * from submissions').all().map(submission => {
-    return {
-      name: submission.name,
-      owner: submission.owner.split(',').map(pup => pup.replace('\r', '')).filter(pup => pup)
-    }
-  });
-  res.render("submissions", { submissions: submissions })
+  res.render("submissions", { submissions: displaySubmissions() })
 }
--- util/db.js
+++ util/db.js
@@ -1,35 +1,40 @@
-import Database from 'better-sqlite3';
+import Database from "better-sqlite3";
 
-export const db = new Database('puppets.db');
+export const db = new Database("puppets.db");
 
-const createTable = `
+export const createTable = `
   CREATE TABLE IF NOT EXISTS puppets(
     'name' text,
     'owner' text
   );
-  CREATE TABLE IF NOT EXISTS submissions(
-    'name' text,
-    'owner' text,
+  CREATE TABLE IF NOT EXISTS submissions (
+    'name' TEXT,
+    'owner' TEXT,
+    'timestamp' INTEGER,
     UNIQUE(name, owner)
   );
   CREATE TABLE IF NOT EXISTS users(
     'username' text,
     'password' text
-  )
-`
-
-db.exec(createTable)
-
-export const submissions = db.prepare('SELECT * from submissions').all().map(submission => {
-  return {
-    name: submission.name,
-    owner: submission.owner.split('\n').map(pup => pup.replace('\r', ''))
-  }
-});
-
-export const displaySubmissions = db.prepare('SELECT * from submissions').all().map(submission => {
-  return {
-    name: submission.name,
-    owner: submission.owner.split(',').map(pup => pup.replace('\r', '')).filter(pup => pup)
-  }
-});
+  );
+`;
+export const displaySubmissions = () => {
+  const createTable = `  CREATE TABLE IF NOT EXISTS submissions (
+    'name' TEXT,
+    'owner' TEXT,
+    'timestamp' INTEGER,
+    UNIQUE(name, owner)
+  );`;
+  db.exec(createTable);
+  return db.prepare("SELECT * from submissions")
+    .all()
+    .map((submission) => {
+      return {
+        name: submission.name,
+        owner: submission.owner
+          .split(",")
+          .map((pup) => pup.replace("\r", ""))
+          .filter((pup) => pup),
+      };
+    });
+};
